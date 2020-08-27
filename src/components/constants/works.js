f345645f17207e4c2521b403ea21c9bc0fd118a9
xomrau <hle3524@gmail.com>
2020-08-27 12:14:00 +1000

    added project list

---
+++ src/components/constants/works.js
@@ -0,0 +1,10 @@
+const works = [
+    {
+        img: "https://user-images.githubusercontent.com/22007153/91196559-a544eb00-e73d-11ea-8008-e79143b5db0a.jpg",
+        title: "Subject Line Assistant",
+        description: "A tool that converts subject line into encoded one.",
+        url: "https://subject-line-assistant.netlify.app/"
+    }
+];
+
+export default works
