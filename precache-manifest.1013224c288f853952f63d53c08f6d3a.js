fa02495206481c4f4e5beeae4c635981427da90b
xomrau <hle3524@gmail.com>
2020-09-08 17:14:54 +1000

    .

---
+++ precache-manifest.1013224c288f853952f63d53c08f6d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab75dcf9b7c54c84801a1487b42b5a65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56152cfaa5cbb8abdd30",
+    "url": "/static/css/main.e8607cab.chunk.css"
+  },
+  {
+    "revision": "d2241165e56046644e0b",
+    "url": "/static/js/2.0470b811.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0470b811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56152cfaa5cbb8abdd30",
+    "url": "/static/js/main.ab10d982.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  }
+]);
